b9abb3dd599f2b7ede0e32a2c7bfd8d9aebb29ca
BuzzWords <buzzwords2017@gmail.com>
2021-02-07 15:47:28 +0900

    Add setting pen color function
    - ペンの色を変える機能を追加

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.modalview import ModalView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.slider import Slider
 from kivy.factory import Factory
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Line
@@ -16,9 +17,14 @@ class MyPaintWidget(Widget):
 
 		self.current_width = 1
 
+		self.red = 255
+		self.green = 255
+		self.blue = 255
+
+
 	def on_touch_down(self, touch):
 		with self.canvas:
-			Color(1, 1, 1)
+			Color(self.red / 255, self.green / 255, self.blue / 255)
 			touch.ud['line'] = Line(points=(touch.x, touch.y), 
 				width=self.current_width, cap='round', joint='round')
 
@@ -43,22 +49,50 @@ class MyPaintLayout(BoxLayout):
 		self.paint = MyPaintWidget()
 		self.action_bar = ActionBar()
 		self.action_view = ActionView()
-		self.action_previous = ActionPrevious()
+		self.action_previous = ActionPrevious(title='Current Color')
+
+		# 色の決定用のモーダルウィンドウ
+		self.pen_color_modal_view = ModalView(size_hint=(0.8, 0.8))
+		self.pen_color_modal_layout = BoxLayout(orientation='vertical')
+		self.current_color_label = Label(text='Current Color')
+		self.r_label = Label(text="Red: "+str(self.paint.red))
+		self.g_label = Label(text="Green: "+str(self.paint.green))
+		self.b_label = Label(text="Blue: "+str(self.paint.blue))
+		self.r_slider = Slider(min=0, max=255, value=self.paint.red, step=1, 
+			on_touch_move=lambda x, y : self.change_pen_color_slider('red', self.r_slider.value))
+		self.g_slider = Slider(min=0, max=255, value=self.paint.green, step=1, 
+			on_touch_move=lambda x, y : self.change_pen_color_slider('green', self.g_slider.value))
+		self.b_slider = Slider(min=0, max=255, value=self.paint.blue, step=1, 
+			on_touch_move=lambda x, y : self.change_pen_color_slider('blue', self.b_slider.value))
+
+		self.pen_color_modal_layout.add_widget(self.current_color_label)
+		self.pen_color_modal_layout.add_widget(self.r_label)
+		self.pen_color_modal_layout.add_widget(self.r_slider)
+		self.pen_color_modal_layout.add_widget(self.g_label)
+		self.pen_color_modal_layout.add_widget(self.g_slider)
+		self.pen_color_modal_layout.add_widget(self.b_label)
+		self.pen_color_modal_layout.add_widget(self.b_slider)
+		self.pen_color_modal_layout.add_widget(Button(
+			text='Close', 
+			on_press=self.pen_color_modal_view.dismiss
+		))
+		self.pen_color_modal_view.add_widget(self.pen_color_modal_layout)
 
 		# 全消去用のモーダルウィンドウ
 		self.clear_modal_view = ModalView(size_hint=(0.7, 0.7))
-		self.modal_layout = BoxLayout(orientation='vertical')
-		self.modal_layout.add_widget(Label(text='Do you really want to clear?'))
-		self.modal_layout.add_widget(Button(
+		self.clear_modal_layout = BoxLayout(orientation='vertical')
+		self.clear_modal_layout.add_widget(Label(text='Do you really want to clear?'))
+		self.clear_modal_layout.add_widget(Button(
 			text='Yes', 
 			on_press=lambda button: [self.paint.canvas.clear(), self.clear_modal_view.dismiss()]
 		))
-		self.modal_layout.add_widget(Button(
+		self.clear_modal_layout.add_widget(Button(
 			text='No', 
 			on_press=lambda button: self.clear_modal_view.dismiss()
 		))
-		self.clear_modal_view.add_widget(self.modal_layout)
+		self.clear_modal_view.add_widget(self.clear_modal_layout)
 
+		self.pen_color_btn = ActionButton(text='Color', on_press=self.pen_color_modal_view.open)
 		self.pen_width_btn = ActionButton(text='width: ' + str(self.paint.current_width))
 		self.clear_btn = ActionButton(text='Clear', on_press=self.clear_modal_view.open)
 
@@ -109,6 +143,7 @@ class MyPaintLayout(BoxLayout):
 
 		# ウィジェットの配置
 		self.action_view.add_widget(self.action_previous)
+		self.action_view.add_widget(self.pen_color_btn)
 		self.action_view.add_widget(self.pen_width_btn)
 		self.action_view.add_widget(self.clear_btn)
 		self.action_bar.add_widget(self.action_view)
@@ -119,6 +154,30 @@ class MyPaintLayout(BoxLayout):
 		self.paint.set_current_width(width)
 		self.pen_width_btn.text = "width: " + str(width)
 		self.pen_width_dropdown.dismiss()
+	
+	def change_pen_color_slider(self, color:str, num:int):
+		if color=='red':
+			self.paint.red = num
+			self.r_label.text = "Red: "+str(self.paint.red)
+		elif color=='green':
+			self.paint.green = num
+			self.g_label.text = "Green: "+str(self.paint.green)
+		elif color=='blue':
+			self.paint.blue = num
+			self.b_label.text = "Blue: "+str(self.paint.blue)
+
+		self.action_previous.color=(
+			self.paint.red/255,
+			self.paint.green/255,
+			self.paint.blue/255,
+		)
+
+		self.current_color_label.color=(
+			self.paint.red/255,
+			self.paint.green/255,
+			self.paint.blue/255,
+		)
+
 
 class lablifepaintApp(App):
 	def build(self):
